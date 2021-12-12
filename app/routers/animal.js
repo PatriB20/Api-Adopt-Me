557f0f339e113a1a762d5d6658b308eb1a248645
esthermoralesM <esthermoraless675@gmail.com>
2021-12-12 14:41:51 +0100

    api animal acabada

--- app/routers/animal.js
+++ app/routers/animal.js
@@ -7,22 +7,37 @@ const mysqlConnection  = require('../../config/sql');
 routes.get('/animal',(req, res)=>{
     console.log('get animal');
 
-    let sql = "SELECT * FROM animal";
+   let sql = "SELECT * FROM animal";
+   // let sql;
+
     let nombreAnimal = req.query.nombreAnimal;
     let sexo = req.query.sexo;
     let tipoAnimal = req.query.tipoAnimal;
     let ingreso = req.query.ingreso;
+    let idProtec=req.query.idProtec;
+    let idAnimal=req.query.idAnimal;
+
+ 
+
+    
 
     nombreAnimal = (!nombreAnimal || nombreAnimal==="")? false : nombreAnimal; 
     sexo = (!sexo || sexo==="")? false : sexo;
     tipoAnimal = (!tipoAnimal || tipoAnimal==="")? false : tipoAnimal;
     ingreso = (!ingreso || ingreso==="")? false : ingreso;
+    idProtec = (!idProtec || idProtec==="")? false : idProtec;
+    idAnimal = (!idAnimal || idAnimal==="")? false : idAnimal;
 
     console.log(nombreAnimal);
     console.log(sexo);
     console.log(tipoAnimal);
     console.log(ingreso);
-    if (nombreAnimal || sexo || tipoAnimal  || ingreso ) {
+
+
+
+
+    if (nombreAnimal || sexo || tipoAnimal  || ingreso || idProtec || idAnimal) {
+   
         sql = sql + " WHERE ";
         if (nombreAnimal) {
             sql = `${sql} nombre='${nombreAnimal}' AND `
@@ -36,20 +51,33 @@ routes.get('/animal',(req, res)=>{
         if (ingreso) {
             sql = `${sql} fecha_ingresso='${ingreso}' AND`;
         }
+
+
+        if(idProtec){
+            sql = `${sql} id_protectora='${idProtec}' AND `
+        }
+
+        if(idAnimal){
+            sql = `${sql} idAnimal='${idAnimal}' AND `
+        }
+
+
+
         sql = sql.substring(0, sql.length - 4);
         sql = `${sql} AND estado='adopcion'`
     }
     else{
         sql = `${sql} WHERE estado='adopcion'`
     }
-    
-    
+
+
     
     mysqlConnection.query(sql, function (err, result) {
 
         if (err)
             console.log(err);
         else {
+            console.log(result);
             res.send(result);
         }
 
@@ -138,12 +166,11 @@ routes.delete('/animal',(req, res)=>{
         if(err)
         console.log(err);
         else{
-            if(result.affectedRows==1){
-            res.send(String(idAnimal));
-            }
-           else
-           console.log(result)
            
+            if(result.affectedRows==1)
+              res.send(String(result.affectedRows));
+             else
+               res.send("0");
         }
     })
 })
